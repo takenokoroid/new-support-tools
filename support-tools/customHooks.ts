2d015d6e495f9ce5670203a7a688f0d3a3a3158b
ifhito <hito01010101@gmail.com>
2022-11-13 14:52:21 +0900

    feat: 検索処理のcustomHookの追加

--- support-tools/customHooks.ts
+++ support-tools/customHooks.ts
@@ -1,5 +1,50 @@
-import {useState} from 'react'
-const Search = () => {
-    const [result, setResult] = useState();
-
-}
+import { ChangeEvent, useState } from 'react';
+import { Result } from 'type/lib';
+export const useSearch = () => {
+  const [userId, setUserId] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [errorStatus, setErrorStatus] = useState(false);
+  const [results, setResults] = useState<Result>([]);
+  const [validMessage, setValidMessage] = useState('');
+  const onSearch = async () => {
+    try {
+      const response = await fetch('/api/search-service', {
+        method: 'POST',
+        body: JSON.stringify({
+          id: userId,
+        }),
+      });
+      if (response.status === 200) {
+        const data: Result = await response.json();
+        setErrorMessage('');
+        setErrorStatus(false);
+        setResults(data);
+      } else {
+        const data = await response.json();
+        setErrorMessage(data.errorMessage);
+        setErrorStatus(true);
+      }
+    } catch (e) {
+      setErrorMessage('エラーです');
+      setErrorStatus(true);
+      console.log('Error', e);
+    }
+  };
+  const idValidation = (id: string) => {
+    const message = (id: string) => {
+      setDisabled(true);
+      if (!id) return 'IDを入力してください';
+      const regex = /^[a-zA-Z]{2}[0-9]{4}$/;
+      if (!regex.test(id)) return '先頭英字2桁+数字4桁で入力してください';
+      setDisabled(false);
+      return '';
+    };
+    setValidMessage(message(id));
+  };
+  const onIdChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserId(e.target.value);
+    idValidation(e.target.value);
+  };
+  return { disabled, userId, errorMessage, errorStatus, results, validMessage, onSearch, onIdChange };
+};
