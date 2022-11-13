69416eff2999b8ac3460d34553538b9b9aa47bae
ifhito <hito01010101@gmail.com>
2022-11-13 14:52:47 +0900

    feat: HTML DOM処理の追加

---
+++ support-tools/pages/components/SearchResult.tsx
@@ -0,0 +1,45 @@
+import { Item } from '../../type/lib';
+import css from '../../styles/SearchResult.module.css';
+const SearchResult = ({ result }: { result: Item }) => (
+  <div className={`container mx-auto ${css.card__wrapper} c-flexbox--row w-96`}>
+    <div className={`c-flexbox--column ${css.card__userwrapper}`}>
+      <div className={`${css.card__userid}`} aria-label={`ユーザIDは${result.user.cgg_id}`}>
+        ID : {result.user.cgg_id}
+      </div>
+      <h2 aria-label={`ユーザIDは${result.user.name}`}>{result.user.name}</h2>
+    </div>
+    <div className={`${css.card__deletedwrapper} relative group`}>
+      <span
+        className={[
+          'whitespace-nowrap',
+          'rounded',
+          'bg-black',
+          'px-2',
+          'py-1',
+          'text-white',
+          'absolute',
+          '-top-12',
+          'left-1/2',
+          '-translate-x-1/2',
+          "before:content-['']",
+          'before:absolute',
+          'before:-translate-x-1/2',
+          'before:left-1/2',
+          'before:top-full',
+          'before:border-4',
+          'before:border-transparent',
+          'before:border-t-black',
+          'opacity-0',
+          'group-hover:opacity-100',
+          'transition',
+          'pointer-events-none',
+        ].join(' ')}
+      >
+        {result.service.allowServiceSentence}
+      </span>
+      <img alt={result.service.allowServiceSentence} src={result.service.allowServiceImage} />
+    </div>
+  </div>
+);
+
+export default SearchResult;
