1f80b6fb95e547c3b71bd36df1489b137096c5a9
takenokoroid <takenokoroid@gmail.com>
2022-11-14 20:47:36 +0900

    feat: 正常系の作成

--- lambda/SampleFunction/main.go
+++ lambda/SampleFunction/main.go
@@ -1,37 +1,103 @@
 package main
 import (
 	"database/sql"
-	"context"
 	"fmt"
-	"net/http"
 	"os"
-	"github.com/aws/aws-lambda-go/events"
+	"strconv"
 	"github.com/aws/aws-lambda-go/lambda"
 	_ "github.com/lib/pq"
 )
 
-type Response struct {
-	Message    string `json:"message"`
-	StatusCode int    `json:"statusCode"`
+type Request struct {
+	Id	string `json:"id"`
 }
-func handleRequest(ctx context.Context, event events.S3Event) (Response, error) {
+
+// type Response struct {
+// 	Message    string `json:"message"`
+// 	StatusCode int    `json:"statusCode"`
+// }
+
+type User struct {
+	CggId string `json:"cgg_id"`
+	Name  string `json:"name"`
+}
+
+type Service struct {
+	Deleted bool `json:"deleted"`
+}
+
+type Response interface{}
+
+func handleRequest(request Request) (Response, error) {
+	if request.Id == "" {
+		Response := struct {
+			errorMessage string `json:"errorMessage"`
+		}{
+			"idが必要です",
+		}
+		err := fmt.Errorf("id is empty")
+		return Response, err
+	}
 	db, err := initDB()
 	if err != nil {
 		fmt.Println(err.Error())
-		return Response{Message: err.Error(), StatusCode: http.StatusInternalServerError}, err
+
+		Response := struct {
+			errorMessage string `json:"errorMessage"`
+		}{
+			"エラーです",
+		}
+		return Response, err
 	}
 
-	cgg_id, err := getCggId(db)
-
+	cgg_id, err := getCggId(db, request.Id)
 	if err != nil {
 		fmt.Println(err.Error())
-		return Response{Message: err.Error(), StatusCode: http.StatusInternalServerError}, err
+		Response := struct {
+			errorMessage string `json:"errorMessage"`
+		}{
+			"データが見つかりません",
+		}
+		return Response, err
+	}
+	
+	deleted, err := getDeleted(db, request.Id)
+	if err != nil {
+		fmt.Println(err.Error())
+		Response := struct {
+			errorMessage string `json:"errorMessage"`
+		}{
+			"データが見つかりません",
+		}
+		return Response, err
 	}
 
-	return Response{
-		Message:    fmt.Sprintf("cgg_id: %s", *cgg_id),
-		StatusCode: http.StatusOK,
-	}, nil
+	name, err := getName(db, request.Id)
+	if err != nil {
+		fmt.Println(err.Error())
+		Response := struct {
+			errorMessage string `json:"errorMessage"`
+		}{
+			"データが見つかりません",
+		}
+		return Response, err
+	}
+	fmt.Println(*cgg_id, *name, *deleted)
+
+	Response := struct {
+		User User `json:"user"`
+		Service Service `json:"service"`
+	}{
+		User{
+			fmt.Sprintf("%s",*cgg_id),
+			fmt.Sprintf("%s",*name),
+		},
+		Service{
+			*deleted,
+		},
+	}
+
+	return Response, nil
 }
 
 func initDB() (*sql.DB, error) {
@@ -53,15 +119,46 @@ func initDB() (*sql.DB, error) {
 }
 
 
-func getCggId(db *sql.DB) (*string, error) {
+func getCggId(db *sql.DB, id string) (*string, error) {
 	var cgg_id string
-	sql := "SELECT cgg_id FROM users LIMIT 1"
-	err := db.QueryRow(sql).Scan(&cgg_id)
+	sql := "SELECT cgg_id FROM users WHERE cgg_id = $1"
+	err := db.QueryRow(sql, id).Scan(&cgg_id)
 	if err != nil {
 		return nil, err
 	}
 
 	return &cgg_id, nil
+}
+
+func getName(db *sql.DB, id string) (*string, error) {
+	var name string
+	sql := "SELECT name FROM users WHERE cgg_id = $1"
+	err := db.QueryRow(sql, id).Scan(&name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &name, nil
+}
+
+func getDeleted(db *sql.DB, id string) (*bool, error) {
+	var user_id int
+	var deleted bool
+
+	sql := "SELECT id FROM users WHERE cgg_id = $1"
+	err := db.QueryRow(sql, id).Scan(&user_id)
+	if err != nil {
+		return nil, err
+	}
+
+	s:=strconv.Itoa(user_id)
+	sql = "SELECT deleted FROM service WHERE user_id = $1"
+	err = db.QueryRow(sql, s).Scan(&deleted)
+	if err != nil {
+		return nil, err
+	}
+
+	return &deleted, nil
 }
 
 func main() {
