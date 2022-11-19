b6aea17cc130767c0456431af877fb93d9acfb56
ifhito <hito01010101@gmail.com>
2022-11-19 12:14:12 +0900

    fixed: nextのbuildが通るための修正

--- support-tools/pages/components/SearchResult.tsx
+++ support-tools/pages/components/SearchResult.tsx
@@ -3,10 +3,10 @@ import css from '../../styles/SearchResult.module.css';
 const SearchResult = ({ result }: { result: Item }) => (
   <div className={`container mx-auto ${css.card__wrapper} md:flex flex-rows w-96`}>
     <div className={`md:flex flex-col ${css.card__userwrapper}`}>
-      <div className={`${css.card__userid}`} aria-label={`ユーザIDは${result.user.cgg_id}`}>
-        ID : {result.user.cgg_id}
+      <div className={`${css.card__userid}`} aria-label={`ユーザIDは${result?.user.cgg_id}`}>
+        ID : {result?.user.cgg_id}
       </div>
-      <h2 aria-label={`ユーザIDは${result.user.name}`}>{result.user.name}</h2>
+      <h2 aria-label={`ユーザIDは${result?.user.name}`}>{result?.user.name}</h2>
     </div>
     <div className={`${css.card__deletedwrapper} relative group`}>
       <span
@@ -35,9 +35,9 @@ const SearchResult = ({ result }: { result: Item }) => (
           'pointer-events-none',
         ].join(' ')}
       >
-        {result.service.allowServiceSentence}
+        {result?.service.allowServiceSentence}
       </span>
-      <img alt={result.service.allowServiceSentence} src={result.service.allowServiceImage} />
+      <img alt={result?.service.allowServiceSentence} src={result?.service.allowServiceImage} />
     </div>
   </div>
 );
